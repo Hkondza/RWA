529831a50fb5f7036d676316b4b15400ebf65ce0
Hkondza <hkondza@algebra.hr>
2025-12-24 18:12:27 +0100

    Izgradnja UI

--- JobFinder/JobFinder.WebApp/obj/Debug/net8.0/JobFinder.WebApp.AssemblyInfo.cs
+++ JobFinder/JobFinder.WebApp/obj/Debug/net8.0/JobFinder.WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobFinder.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6beb75bd275cc6142ca0379a2d5a9a3dd86ecb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3ff714fae6230c78a9cde6b75dd41b972c4b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobFinder.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobFinder.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
