693afd64f731eb3a0f7ffe8cb220493ad4385b5a
Hkondza <hkondza@algebra.hr>
2026-01-02 23:06:23 +0100

    UI update

--- JobFinder/JobFinder.WebAPI/obj/Debug/net8.0/JobFinder.WebAPI.AssemblyInfo.cs
+++ JobFinder/JobFinder.WebAPI/obj/Debug/net8.0/JobFinder.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobFinder.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+902eb570f7f6ee2646ffe2ef2a43ec33e6a560a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+046711336a74f975b2672b6de1c87c37455303f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobFinder.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobFinder.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
